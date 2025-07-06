bd9a43a2cd82e222af2d484eed1544b01b50df25
OBA <151268342+Edunetx@users.noreply.github.com>
2025-07-06 08:24:13 +0100

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-// supabase.js
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const supabase = createClient(
-  "https://edunetx.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGFmbWFycnl0aXhqZ2Vma2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NzM3OTksImV4cCI6MjA2NzM0OTc5OX0.baDfihnD_fstSU7MkdAIiVSQKmTbrX3HvSuRKZs5bIA"
-);
